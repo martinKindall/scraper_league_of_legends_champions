f8920241f7caf495d5702c647e5f2434c4c9fa4a
martinKindall <mart256@gmail.com>
2020-05-10 23:29:23 -0400

    Added dotenv to properly use .env and set db login data.

--- services/ChampionService.py
+++ services/ChampionService.py
@@ -1,12 +1,19 @@
 import typing
 import mysql.connector as mariadb
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 if typing.TYPE_CHECKING:
 	from dataClasses.Champion import Champion
 
 class ChampionService:
-	mariadb_connection = \
-		mariadb.connect(user='python_user', password='some_pass', database='employees')
+	mariadb_connection = mariadb.connect(
+		user=os.getenv("DB_USER"),
+		password=os.getenv("DB_PASS"),
+		database=os.getenv("DB_PASS"))
 
 	def save(self, champion: 'Champion'):
 		pass
